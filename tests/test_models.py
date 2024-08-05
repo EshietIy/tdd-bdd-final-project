fe5031f0fe0404a17d75cdcb3a7ddcaef17292ee
EshietIy <eshietinyang@gmail.com>
2024-08-05 11:53:11 -0400

    first commit

--- tests/test_models.py
+++ tests/test_models.py
@@ -35,6 +35,7 @@ DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
 
+logger = logging.getLogger("flask.app")
 
 ######################################################################
 #  P R O D U C T   M O D E L   T E S T   C A S E S
@@ -104,3 +105,95 @@ class TestProductModel(unittest.TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+    def test_read_a_product(self):
+        """It should Read a Product"""
+        product = ProductFactory()
+        # Set the ID of the product object to None and then call the create() method on the product.
+        # Assert that the ID of the product object is not None after calling the create() method.
+        # Fetch the product back from the system using the product ID and store it in found_product
+        # Assert that the properties of the found_product match with the properties of the original product object, such as id, name, description and price.
+        logger.info("Reading product %s", product)
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        products = Product.all()
+        new_product = products[0]
+        self.assertEqual(new_product.name, product.name)
+        self.assertEqual(new_product.description, product.description)
+        self.assertEqual(Decimal(new_product.price), product.price)
+        self.assertEqual(new_product.id, product.id)
+           
+
+    def test_update_a_product(self):
+        """It should Update a Product"""
+        product = ProductFactory()
+        # Set the ID of the product object to None and then call the create() method on the product.
+        # Log the product object again after it has been created to verify that the product was created with the desired properties.
+        # Assert that the ID of the product object is not None after calling the create() method.
+        # Update the product in the system with the new property values using the update() method.
+        # Assert that the id is same as the original id but description property of the product object has been updated correctly after calling the update() method.
+        # Fetch all the product back from the system.
+        # Assert the length of the products list is equal to 1 to verify that after updating the product, there is only one product in the system.
+        # Assert that the fetched product has id same as the original id.
+        # Assert that the fetched product has the updated description.
+        product = ProductFactory()
+        logger.info("Product created %s for description modififcation", product)
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        description = "This description has been modified"
+        product.description = description
+        product.update()
+        products = Product.all()
+        new_product = products[0]
+        self.assertEqual(new_product.id, product.id)
+        self.assertEqual(new_product.description, description)
+
+    def test_delete_a_product(self):
+        """It should Delete a Product"""
+        product = ProductFactory()
+        # Call the create() method on the product to save it to the database.
+        # Assert  if the length of the list returned by Product.all() is equal to 1, to verify that after creating a product and saving it to the database, there is only one product in the system.
+        # Call the delete() method on the product object, to remove the product from the database.
+        # Assert if the length of the list returned by Product.all() is now equal to 0, indicating that the product has been successfully deleted from the database.
+        product.create()
+        products = Product.all()
+        #ensure product was saved
+        new_product = products[0]
+        self.assertEqual(new_product.id, product.id)
+        self.assertEqual(len(products), 1)
+        #delete product for database
+        product.delete()
+        empty_products = Product.all()
+        self.assertEqual(len(empty_products), 0)
+
+    def test_list_all_products(self):
+        """It should List all Products in the database"""
+        products = Product.all()
+        # Assert if the products list is empty, indicating that there are no products in the database at the beginning of the test case.
+        # Use for loop to create five Product objects using a ProductFactory() and call the create() method on each product to save them to the database.
+        # Fetch all products from the database again using product.all()
+        # Assert if the length of the products list is equal to 5, to verify that the five products created in the previous step have been successfully added to the database.
+        self.assertEqual(len(products), 0)
+
+        # creating 5 products
+        for _ in range(5):
+            new_product = ProductFactory()
+            new_product.create()
+        products = Product.all()
+        self.assertEqual(len(products), 5)
+
+    def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        # Use a for loop to iterate over the products list and call the create() method on each product to save them to the database.
+        # Retrieve the name of the first product in the products list.
+        # Use a list comprehension to filter the products based on their name and then use len() to calculate the length of the filtered list, and use the variable called count to hold the number of products that match the name.
+        # Call the find_by_name() method on the Product class to retrieve products from the database that have the specified name.
+        # Assert if the count of the found products matches the expected count.
+        # Use a for loop to iterate over the found products and assert that each product's name matches the expected name, to ensure that all the retrieved products have the correct name.
+        for product in len(products):
+            product.create()
+        first_product = products[0].name
+        count = len([product.name : for product in products])
+        
